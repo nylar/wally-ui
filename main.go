bfe11c4f1d4575292cacb5b4f7302b4939ea75e5
Nylar <me@nylar.io>
2014-12-20 22:37:57 -0500

    Barebones app

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"text/template"
+
+	rdb "github.com/dancannon/gorethink"
+	"github.com/fatih/color"
+	"github.com/nylar/wally"
+)
+
+var session *rdb.Session
+
+func init() {
+	var err error
+	session, err = rdb.Connect(rdb.ConnectOpts{
+		Address:  os.Getenv("RETHINKDB_URL"),
+		Database: wally.Database,
+	})
+	if err != nil {
+		color.Set(color.FgRed)
+		log.Fatalln(err.Error())
+		color.Unset()
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	res := new(wally.Results)
+	ctx := struct {
+		Res   *wally.Results
+		Query string
+	}{
+		res,
+		"",
+	}
+
+	if r.Method == "POST" {
+		var err error
+		query := r.PostFormValue("query")
+		res, err = wally.Search(query, session)
+		ctx.Res = res
+		ctx.Query = query
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+	}
+
+	tmpl, err := template.ParseFiles("template.html")
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	if err := tmpl.ExecuteTemplate(w, "template.html", ctx); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8008", nil)
+}
